aa7592deb40adf74dc45f2e01f1a376af50ac58f
Hema Satyanarayana <hema.satyanarayana@apxor.com>
2024-10-15 17:50:37 +0530

    Added react router dom

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-const parent = React.createElement(
-  "div",
-  { id: "heading" },
-  React.createElement("div", {}, [
-    React.createElement("h1", { id: "child" }, "I'm a react developer now"),
-    React.createElement("h2", { id: "child" }, "I'm a react developer now"),
-  ])
-);
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const root = createRoot(document.getElementById("root"));
+root.render(<>hello</>);
